e4f595626369f29641eea5437b5dd00aeffb7294
catrubens <etu22351@student.henallux.be>
2015-09-29 17:04:47 +0200

    Labo 1

--- ConsoleApplicationLabo1/ConsoleApplicationLabo1/Program.cs
+++ ConsoleApplicationLabo1/ConsoleApplicationLabo1/Program.cs
@@ -26,67 +26,70 @@ namespace Labo1
             Activity activity10 = new Activity("musique", true);
             Activity activity11 = new Activity("basket", true);
 
-           /* eleve1.addActivity(activity1);
-            eleve1.addActivity(activity2);
-            eleve1.AddEvaluation(evaluation: 'T', titre: "vélo");
+            /* eleve1.addActivity(activity1);
+             eleve1.addActivity(activity2);
+             eleve1.AddEvaluation(evaluation: 'T', titre: "vélo");
 
-            eleve2.addActivity(activity3);
-            eleve2.addActivity(activity2);
+             eleve2.addActivity(activity3);
+             eleve2.addActivity(activity2);
 
-            eleve2.AddEvaluation("piscine", 'R');
-            eleve2.AddEvaluation("marche", 'T');
-            eleve2.AddEvaluation("vélo");
+             eleve2.AddEvaluation("piscine", 'R');
+             eleve2.AddEvaluation("marche", 'T');
+             eleve2.AddEvaluation("vélo");
 
-   //         System.Console.Write(eleve1);
-            System.Console.Write("\n\n");
-            System.Console.Write(eleve2);
-            System.Console.ReadKey();
-            System.Console.Write("\n\n");
-            System.Console.Write(eleve2.GetEvatuation("vélo"));
-            System.Console.ReadKey();
-            System.Console.Write("\n\n");
+    //         System.Console.Write(eleve1);
+             System.Console.Write("\n\n");
+             System.Console.Write(eleve2);
+             System.Console.ReadKey();
+             System.Console.Write("\n\n");
+             System.Console.Write(eleve2.GetEvatuation("vélo"));
+             System.Console.ReadKey();
+             System.Console.Write("\n\n");
+             * 
+ //Variable anonyme
+             System.Console.Write("Variable anonyme \n");
+             List<Pupil> listePupil = new List<Pupil>()
+             {
+                 new Pupil("Laura",13,3),
+                 new Pupil("Schumi",11,2),
+                 new Pupil("Tomi",9),
+                 new Pupil("Sacha",5),
+                 new Pupil("Samy",4),
+                 new Pupil("Fred",6,1),
+                 new Pupil("Valérie",5),
+                 new Pupil("Alonso",3),
+             };
 
-            System.Console.Write("Variable anonyme \n");
-            List<Pupil> listePupil = new List<Pupil>()
-            {
-                new Pupil("Laura",13,3),
-                new Pupil("Schumi",11,2),
-                new Pupil("Tomi",9),
-                new Pupil("Sacha",5),
-                new Pupil("Samy",4),
-                new Pupil("Fred",6,1),
-                new Pupil("Valérie",5),
-                new Pupil("Alonso",3),
-            };
+             var pupilGrade1Plus6 = from eleve in listePupil
+                                    where eleve.Grade == 1 && eleve.Age > 6
+                                    select eleve;
 
-            var pupilGrade1Plus6 = from eleve in listePupil
-                                   where eleve.Grade == 1 && eleve.Age > 6
-                                   select eleve;
+             if(pupilGrade1Plus6 != null)
+                 foreach (var eleves in pupilGrade1Plus6)
+                 {
+                     System.Console.Write(eleves.ToString() + "\n");
+                 }
 
-            if(pupilGrade1Plus6 != null)
-                foreach (var eleves in pupilGrade1Plus6)
-                {
-                    System.Console.Write(eleves.ToString() + "\n");
-                }
+             System.Console.ReadKey();
+             System.Console.Write("\n\n");
 
-            System.Console.ReadKey();
-            System.Console.Write("\n\n");
+// Classe statique
+             System.Console.Write("Classe statique \n");
+             eleve3.addActivity(activity1);
+             eleve3.addActivity(activity2);
+             eleve3.addActivity(activity3);
+             eleve3.addActivity(activity4);
+             eleve3.addActivity(activity5);
+             eleve3.addActivity(activity6);
+             eleve3.addActivity(activity7);
+             //eleve3.addActivity(activity8);
+             eleve3.addActivity(activity9);
+             eleve3.addActivity(activity10);
+             eleve3.addActivity(activity11);
+             System.Console.Write(eleve3);
+             System.Console.ReadKey();
 
-            System.Console.Write("Classe statique \n");
-            eleve3.addActivity(activity1);
-            eleve3.addActivity(activity2);
-            eleve3.addActivity(activity3);
-            eleve3.addActivity(activity4);
-            eleve3.addActivity(activity5);
-            eleve3.addActivity(activity6);
-            eleve3.addActivity(activity7);
-            //eleve3.addActivity(activity8);
-            eleve3.addActivity(activity9);
-            eleve3.addActivity(activity10);
-            eleve3.addActivity(activity11);
-            System.Console.Write(eleve3);
-            System.Console.ReadKey();*/
-
+//Lambda
             System.Console.Write("Lambda \n");
             List<Pupil> listePupil = new List<Pupil>()
             {
@@ -100,17 +103,49 @@ namespace Labo1
                 new Pupil("Alonso",3),
             };
 
-            /*var pupilGrade1Plus6 = from eleve in listePupil
-                                   where eleve.Grade == 1 && eleve.Age > 6
-                                   select eleve;*/
-
-            listePupil.Where(pupilGrade1Plus6 => pupilGrade1Plus6.Age > 6 && pupilGrade1Plus6.Grade == 1);
-
-            if (listePupil != null)
-                foreach (var eleves in listePupil)
+            var pupilGrade1Plus6 = listePupil.Where(pupil => (pupil.Age > 6 && pupil.Grade == 1));
+            if (pupilGrade1Plus6 != null)
+                foreach (var eleves in pupilGrade1Plus6)
                 {
                     System.Console.Write(eleves.ToString() + "\n");
                 }
+            */
+//Cavariance
+            System.Console.Write("Cavariance \n");
+            List<Pupil> listPupils = new List<Pupil>() { 
+                new Pupil("Léo",18,4),
+                new Pupil("Bernie",12,2),
+                new Pupil("Celine",15,3),
+                new Pupil("Homer",5),
+            };
+
+            List<Person> listPerson = new List<Person>() { 
+                new Pupil("Bart",18),
+                new Pupil("Lisa",12),
+                new Pupil("Marge",15),
+                new Pupil("Magie",12),
+            };
+
+            var listFusion = listPerson.Union(listPupils);
+            foreach (var pers in listFusion)
+            {
+                System.Console.Write(pers.ToString() + "\n");
+            }
+
+            List<Pupil> listPupilsDuplicated = new List<Pupil>() { 
+                new Pupil("Jules",18),
+                new Pupil("Nico",15),
+                new Pupil("Jules",18),                
+                new Pupil("Nico",15),
+            };
+            System.Console.ReadKey();
+            System.Console.Write("Doublon \n");
+            IEnumerable<Pupil> listPupilsNoDuplicated = listPupilsDuplicated.Distinct<Pupil>(new PersonComparer());
+
+            foreach (var pers in listPupilsNoDuplicated)
+            {
+                System.Console.Write(pers.ToString() + "\n");
+            }
 
             System.Console.ReadKey();
            
---
+++ ConsoleApplicationLabo1/Pupil/PersonComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo1
+{
+    public class PersonComparer : IEqualityComparer<Person>
+    {
+        public bool Equals(Person p1, Person p2)
+        {
+            if (p1.Name.Equals(p2.Name) && p1.Age == p2.Age)
+                return true;
+
+            return false;
+        }
+        public int GetHashCode(Person p)
+        {
+            return (int)Math.Pow(p.Name.GetHashCode(),p.Age);
+        }
+    }
+}
