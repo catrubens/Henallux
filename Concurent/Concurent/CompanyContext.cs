379477777bd47396becd998db05b1631da36939f
Catubens <antoine.lesire@hotmail.com>
2015-11-21 10:44:47 +0100

    maj

---
+++ Concurent/Concurent/CompanyContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concurent
+{
+    public class CompanyContext : DbContext
+    {
+
+        public DbSet<Customer> Customers
+        {
+            get;
+            set;
+        }
+      
+       // public CompanyContext(string ConnectionString)
+        //    : base()
+        public CompanyContext()
+            : base(@"Data Source=vm-sql.iesn.be\Stu3IG;Initial Catalog=DBIG3A2;User ID=IG3A2;Password=***********")
+        {
+
+        }
+
+       
+    }   
+}
---
+++ Concurent/TestUnitaire/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity;
+using Concurent;
+
+namespace TestUnitaire
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<CompanyContext>());
+
+        }
+    }
+}
